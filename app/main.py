e38aaed5a86d84f4fb01bae8db36992a71af866c
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-22 10:57:32 +0200

    Data processing in pandas

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 import pandas as pd
+import uvicorn
+from models import cut_categorize
 
 app = FastAPI()
 
@@ -7,4 +9,8 @@ app = FastAPI()
 def upload_file(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
     file.file.close()
+    cut_categorize(df)
     return {"filename": file.filename}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
