59cf5e21c2cba8d0edd3748d8fe1e55a6fe74e73
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-22 12:32:48 +0200

    run yaml's

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ from models import cut_categorize
 
 app = FastAPI()
 
+
 @app.post("/upload")
 def upload_file(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
     file.file.close()
     cut_categorize(df)
     return {"filename": file.filename}
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ def cut_categorize(df):
     df['risk_level'] = pd.cut(df['range_km'], bins=bins, labels=labels)
     return NaN_cleaning(df)
 
+
 def NaN_cleaning(df):
     df['manufacturer'] = df['manufacturer'].fillna('Unknown')
     return df
